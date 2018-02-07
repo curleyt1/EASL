6d39ddd528ff0aa80e7633c1a6fa153bea354ea9
Terrance Curley <curleyt1@wit.edu>
2018-02-07 10:18:43 -0500

    Migrate previous Signhelper project
    Created new django application for eASL and brought over existing work
    done for Signhelper. Organized new git repository and updated .gitignore

---
+++ easl/webApp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from webApp import views
+
+urlpatterns = [
+    url(r'^$', views.StartPageView.as_view()),
+    url(r'^index/$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()),
+]

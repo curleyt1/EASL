1136525fff189c5147be24c352bb9d8842f628d6
Terrance Curley <curleyt1@wit.edu>
2018-02-15 15:07:55 -0500

    add age and gender to student detail page

--- easl/webApp/models.py
+++ easl/webApp/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 
 class Student(models.Model):
@@ -5,3 +6,16 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
+
+    def age(self):
+        today = date.today()
+
+        try:
+            birthday = self.date_of_birth.replace(year=today.year)
+        except ValueError:
+            birthday = self.date_of_birth.replace(year=today.year)
+
+        if birthday > today:
+            return today.year - self.date_of_birth.year - 1
+        else:
+            return today.year - self.date_of_birth.year

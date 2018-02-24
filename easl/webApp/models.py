ca0a9f8b9f8a1e525b61282254457ce5501f64c9
Terrance Curley <curleyt1@wit.edu>
2018-02-24 14:37:34 -0500

    Define action model.
    Add action class to models.py. currently only consists of an action and
    date/time

--- easl/webApp/models.py
+++ easl/webApp/models.py
@@ -1,5 +1,11 @@
 from datetime import date
+from datetime import datetime
 from django.db import models
+
+class Action(models.Model):
+    ACTION_CHOICES = [('S', 'Snack'), ('P', 'Play'), ('O', 'Outside')]
+    time = models.DateTimeField()
+    action = models.CharField(max_length=1, choices=ACTION_CHOICES)
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
@@ -19,4 +25,3 @@ class Student(models.Model):
             return today.year - self.date_of_birth.year - 1
         else:
             return today.year - self.date_of_birth.year
-

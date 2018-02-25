ff181466badfd3a066478ef675fb03f16635cb05
Terrance Curley <curleyt1@wit.edu>
2018-02-25 17:48:08 -0500

    Actions updated with foreign key. migrated.

--- easl/webApp/models.py
+++ easl/webApp/models.py
@@ -2,11 +2,6 @@ from datetime import date
 from datetime import datetime
 from django.db import models
 
-class Action(models.Model):
-    ACTION_CHOICES = [('S', 'Snack'), ('P', 'Play'), ('O', 'Outside')]
-    time = models.DateTimeField()
-    action = models.CharField(max_length=1, choices=ACTION_CHOICES)
-
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -25,3 +20,9 @@ class Student(models.Model):
             return today.year - self.date_of_birth.year - 1
         else:
             return today.year - self.date_of_birth.year
+
+class Action(models.Model):
+    ACTION_CHOICES = [('S', 'Snack'), ('P', 'Play'), ('O', 'Outside')]
+    time = models.DateTimeField()
+    action = models.CharField(max_length=1, choices=ACTION_CHOICES)
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)

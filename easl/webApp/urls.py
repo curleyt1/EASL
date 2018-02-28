510632e752746ef4a1c57002ae40a9978024799c
Terrance Curley <curleyt1@wit.edu>
2018-02-28 13:00:59 -0500

    remove unused registration confirmation

--- easl/webApp/urls.py
+++ easl/webApp/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'registration_page', views.registration_page, name='registration_page'),
     url(r'^actions/$', views.action_log, name='action_log'),
     url(r'^directory/$', views.directory, name='directory'),
-    url(r'^registration_confirmation/$', views.registration_confirmation, name='registration_confirmation'),
 
 ]
